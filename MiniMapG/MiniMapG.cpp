dcc78e51e8b1125a24fc9fb98d12925229d0a317
Дмитрий Вожегов <vodimitriy@hotmail.com>
2020-06-12 21:50:07 +0300

    Реализован поиск координат участка мини-карты

--- MiniMapG/MiniMapG.cpp
+++ MiniMapG/MiniMapG.cpp
@@ -9,13 +9,24 @@
 
 using std::cin;  using std::cout; using std::max;
 
+struct point // точка на мини карте
+{
+	int x, y; // координаты точки на мини карте
+};
+
+struct miniMapPart // прямоугольный участвок мини карты
+{
+	int mapArea; // площадь участка мини карты
+	point TopLeftCorner, BottomLeftCorner; // левый верхний угол и нижний правый угол участка мини карты
+};
+
 //Статический массив для проверки алгоритма
 char a[5][6] =
 {
 {0,0,1,0,1,0},
 {1,0,1,0,1,0},
-{1,1,1,0,1,0},
-{1,1,1,0,1,1},
+{1,1,1,0,0,0},
+{1,1,1,0,0,0},
 {1,1,1,1,1,1}
 };
 
@@ -27,8 +38,20 @@ char d1[6] = { 0, 0, 0, 0, 0, 0 };
 int main()
 {
 	int n = 5, m = 6; // высота и ширина матрицы
-	int ans = 0; //результат
-	std::stack<int> st; //стек
+	miniMapPart answer; //результат, включающий координаты участка мини карты и его площадь
+	int height; // высота участка мини карты
+	int width; // высота участка мини карты
+
+	// обнулим все значения в ответе
+	answer.mapArea = 0;
+	answer.TopLeftCorner.x = 0;
+	answer.TopLeftCorner.y = 0;
+	answer.BottomLeftCorner.x = 0;
+	answer.BottomLeftCorner.y = 0;
+
+	std::stack<int> st; //создание стека
+
+	// цикл по строкам исходного массива
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
@@ -56,9 +79,23 @@ int main()
 			st.push(j);
 		}
 		for (int j = 0; j < m; ++j)
-			ans = max(ans, (i - d[j]) * (d2[j] - d1[j] - 1));
-	}
+		{
+			height = i - d[j];
+			width = (d2[j] - d1[j] - 1);
 
-	cout << ans;
+			// если текущее значение площади меньше нового, то обновить текущее новым значением
+			if (answer.mapArea < (height * width)) 
+			{
+				answer.mapArea = (height * width);
+				answer.TopLeftCorner.x = d1[j]+1;
+				answer.TopLeftCorner.y = d[j]+1;	
+				answer.BottomLeftCorner.x = d2[j] - 1;
+				answer.BottomLeftCorner.y = i;
+			}
+		}
+
+	}
+	//вывод ответа на консоль
+	printf("Area=%d, TopLeftCorner(X=%d,Y=%d), BottomLeftCorner(X=%d,Y=%d)", answer.mapArea, answer.TopLeftCorner.x, answer.TopLeftCorner.y, answer.BottomLeftCorner.x, answer.BottomLeftCorner.y);
 }
 
